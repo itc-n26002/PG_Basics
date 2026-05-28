0e4b3ed11973c6eaeee40164ff56d74bea48148e
itc-n26002 <n26002@std.it-college.ac.jp>
2026-05-28 07:17:47 +0000

    第３章演習終了

---
+++ Chapter03/challenge3-03.py
@@ -0,0 +1,10 @@
+x = 5
+
+if x < 10:
+    print("う")
+ 
+elif 10 <= x <= 25:
+    print("え")
+
+else: 
+    print("お")
---
+++ Chapter03/challenge3-06.py
@@ -0,0 +1,7 @@
+age = 100
+
+if age % 2 == 0:
+    print("ありがとう")
+
+else:
+    print("厳しい")
