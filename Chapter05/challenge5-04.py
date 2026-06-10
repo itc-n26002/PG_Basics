b71203f9ea3d17018d8fd1db9a16d4d592975e49
itc-n26002 <n26002@std.it-college.ac.jp>
2026-06-10 07:57:18 +0000

    チャレンジ問題

---
+++ Chapter05/challenge5-04.py
@@ -0,0 +1,12 @@
+Amuro = {
+        "出身校": "西原高校",
+        "飼っているペット": "猫"
+}
+
+key = input("入力してください:")
+if key in Amuro:
+    print(Amuro[key])
+else:
+    print("見つかりません")
+
+
---
+++ Chapter05/challenge5-06.py
@@ -0,0 +1,5 @@
+list_animal = ["犬","猿","猫","兎","犬","猫"]
+set_animal = {"犬","猿","猫","兎","犬","猫"}
+
+print(list_animal)
+print(set_animal)
